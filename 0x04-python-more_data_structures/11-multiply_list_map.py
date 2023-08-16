3b3436c24bb2b998e84d7c430849ee57921a9802
Awele688 <annappah84@gmail.com>
2023-08-16 15:41:23 -0700

    update

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -4,4 +4,4 @@ number without using any loops."""
 
 
 def multiply_list_map(my_list=[], number=0):
-    return (list(map((lambda j: j * number), my_list)))
+    return (list(map((lambda i: i * number), my_list)))
